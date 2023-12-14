7d2ba794da6a92c8141833be8ea2e02c51c772ff
David García <dgarciagonzalez@altavoz.net>
2023-12-14 17:51:26 -0300

    cambiar dir pruebas a service

--- src/test/java/com/prueba/bcitest/service/Registrar.java
+++ src/test/java/com/prueba/bcitest/service/Registrar.java
@@ -1,4 +1,4 @@
-package com.prueba.bcitest;
+package com.prueba.bcitest.service;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.Order;
